ff9ad7e8535f99ff79da9a1b2b2beefa7b15a9b4
Van <sung8881@naver.com>
2023-06-14 15:06:48 +0900

    BE_Feat: 홈 컨트롤러 추가

---
+++ src/main/java/com/project/mainproject/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.project.mainproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class HomeController {
+}
